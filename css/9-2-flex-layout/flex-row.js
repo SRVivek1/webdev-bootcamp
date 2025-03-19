0ffb18f73acedeecf1e7a7d15b067fc56316bd24
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2025-03-19 07:13:11 +0530

    added flex properties specific for container items

--- css/9-2-flex-layout/flex-row.js
+++ css/9-2-flex-layout/flex-row.js
@@ -113,7 +113,7 @@ alignContent.addEventListener("change", function () {
       msg = "Align at top of container.";
       break;
     case "center":
-        msg = "Align in center of container from top to bottom.";
+      msg = "Align in center of container from top to bottom.";
       break;
     case "flex-end":
       msg = "Align at bottom of container.";
@@ -130,22 +130,64 @@ alignContent.addEventListener("change", function () {
     default:
       msg = "Unknow property : " + alignContent.value;
   }
-  if(!msg.startsWith("Unknow property")) msg = msg + " Note: doesn't work with nowrap.";
+  if (!msg.startsWith("Unknow property"))
+    msg = msg + " Note: doesn't work with nowrap.";
   updatePropDesc(msg);
 });
-
-
 
 const cmwElements = document.getElementsByClassName("container");
 // Ensure at least one element exists
 if (cmwElements.length > 0) {
-    const cmw = cmwElements[0].offsetWidth; // Access the first element's width
-    document.getElementById("container-width-input").value = cmw;
-    console.log(cmw)
+  const cmw = cmwElements[0].offsetWidth; // Access the first element's width
+  document.getElementById("container-width-input").value = cmw;
+  console.log(cmw);
 }
 
 //update container width
 const cwi = document.getElementById("container-width-input");
 cwi.addEventListener("change", function () {
-    cmwElements[0].style.width = cwi.value + "px";
+  cmwElements[0].style.width = cwi.value + "px";
 });
+
+// flex item properties
+
+// flex-basis - Defines the starting width of an item before flex-grow or flex-shrink applies.
+const flexBasis = document.getElementById("flex-basis");
+flexBasis.addEventListener("change", function () {
+  // implement the style on very first and last item in flex container
+  let t = document.getElementsByClassName("box");
+  t[t.length - 1].style.flexBasis = flexBasis.value + "px";
+  t[0].style.flexBasis = flexBasis.value + "px";
+
+  let msg =
+    "Specifies the initial main size of a flex item before any available space is distributed.";
+  updatePropDesc(msg);
+});
+
+// flex-grow - Defines how much an item should grow relative to others.
+const flexGrow = document.getElementById("flex-grow");
+flexGrow.addEventListener("change", function () {
+  // implement the style on very first and last item in flex container
+  let t = document.getElementsByClassName("box");
+  t[t.length - 1].style.flexGrow = flexGrow.value / 2;
+  t[0].style.flexGrow = flexGrow.value;
+
+  let msg =
+    "Grows flex item relative to other flex items when there's available space in flex container.";
+  updatePropDesc(msg);
+});
+
+// flex-shrink - Defines how much an item should shrink if the container gets smaller.
+const flexShrink = document.getElementById("flex-shrink");
+flexShrink.addEventListener("change", () => {
+  // implement the style on very first and last item in flex container
+  let t = document.getElementsByClassName("box");
+  t[t.length - 1].style.flexShrink = elem.value / 2;
+  t[0].style.flexShrink = elem.value;
+
+  let msg =
+    "Defines how much a flex item will shrink relative to other items when there is insufficient in the flex container.";
+  updatePropDesc(msg);
+});
+
+// align-self (Overrides align-items for Individual Items) - Allows an individual item to have a different vertical alignment.

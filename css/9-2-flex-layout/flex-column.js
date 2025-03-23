eb2ca5550c18bd7e4f98d2c49ef86f32e85a0425
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2025-03-23 16:40:30 +0530

    flex cotainer row direction

--- css/9-2-flex-layout/flex-column.js
+++ css/9-2-flex-layout/flex-column.js
@@ -1,27 +1,246 @@
+// default flex direction is set to row
+const dmsg =
+  "By Default flex-direction is set to row, change the value from menu to see the impact.";
+updatePropDesc(dmsg);
+
+function updatePropDesc(msg) {
+  let t = document.getElementById("fprop-desc");
+  t.innerText = msg;
+}
+
 // flexDirection - Select the dropdown dynamically
 const flexDirection = document.getElementById("flex-direction");
 // Add event listener for the 'change' event
-flexDirection.addEventListener("change", function() {
-    const ele = document.getElementsByClassName("container")[0];
-    ele.style.flexDirection = flexDirection.value;
-});
+flexDirection.addEventListener("change", function () {
+  const ele = document.getElementsByClassName("container")[0];
+  ele.style.flexDirection = flexDirection.value;
 
-// justify content
-const justifyContent = document.getElementById("justify-content");
-justifyContent.addEventListener("change", function() {
-    document.getElementsByClassName("container")[0].style.justifyContent = justifyContent.value;
+  //
+  const msg =
+    flexDirection.value === "column"
+      ? flexDirection.value + ": show flex items from top to bottom."
+      : flexDirection.value + ": show flex items from bottom to top.";
+  updatePropDesc(msg);
 });
-
 
 //align-items
 const alignItems = document.getElementById("align-items");
-alignItems.addEventListener("change", function() {
-    document.getElementsByClassName("container")[0].style.alignItems = alignItems.value;
+alignItems.addEventListener("change", function () {
+  document.getElementsByClassName("container")[0].style.alignItems =
+    alignItems.value;
+
+  let msg = "";
+  switch (alignItems.value) {
+    case "flex-start":
+      msg = "Align items at top of flex container.";
+      break;
+    case "center":
+      msg = "Align items in center of flex container.";
+      break;
+    case "flex-end":
+      msg = "Align items at bottom of flex container.";
+      break;
+    case "baseline":
+      msg = "Align items at top of flex container.";
+      break;
+    case "stretch":
+      msg = "Not working as of now, check for errors.";
+      break;
+    default:
+      msg = "Unknown property: " + alignItems.value;
+  }
+  updatePropDesc(msg);
 });
 
+/** Refactored til here */
+
+// justify content
+const justifyContent = document.getElementById("justify-content");
+justifyContent.addEventListener("change", function () {
+  document.getElementsByClassName("container")[0].style.justifyContent =
+    justifyContent.value;
+
+  console.log(justifyContent.value);
+  let msg = "";
+  switch (justifyContent.value) {
+    case "flex-start":
+      msg = "Align items from left to right on main axis (row)";
+      break;
+    case "center":
+      msg = "Align items in center on main axis (row)";
+      break;
+    case "flex-end":
+      msg = "Align items from right to left on main axis (row)";
+      break;
+    case "space-between":
+      msg = "Place items from start to end adding space in between.";
+      break;
+    case "space-around":
+      msg = "Adds spaces before and after all items.";
+      break;
+    case "space-evenly":
+      msg = "Adds even space before and after all items.";
+      break;
+    default:
+      msg = "unknown property: " + justifyContent.value;
+  }
+  updatePropDesc(msg);
+});
+
+//flex-wrap
+const flexWrap = document.getElementById("flex-wrap");
+flexWrap.addEventListener("change", function () {
+  document.getElementsByClassName("container")[0].style.flexWrap =
+    flexWrap.value;
+  let msg = "";
+  switch (flexWrap.value) {
+    case "wrap":
+      msg = "wrap the elements in container width.";
+      break;
+    case "nowrap":
+      msg = "Donot wrap the elements in container.";
+      break;
+    case "wrap-reverse":
+      msg = "wrap the elements in container width from top to bottom";
+      break;
+    default:
+      msg = "Unknow property : " + flexWrap.value;
+  }
+  updatePropDesc(msg);
+});
 
 //align-content
 const alignContent = document.getElementById("align-content");
-alignContent.addEventListener("change", function() {
-    document.getElementsByClassName("container")[0].style.alignContent = alignContent.value;
+alignContent.addEventListener("change", function () {
+  document.getElementsByClassName("container")[0].style.alignContent =
+    alignContent.value;
+
+  let msg = "";
+  switch (alignContent.value) {
+    case "flex-start":
+      msg = "Align at top of container.";
+      break;
+    case "center":
+      msg = "Align in center of container from top to bottom.";
+      break;
+    case "flex-end":
+      msg = "Align at bottom of container.";
+      break;
+    case "space-between":
+      msg = "Add spaces in between items.";
+      break;
+    case "space-around":
+      msg = "Add spaces in around items.";
+      break;
+    case "stretch":
+      msg = "Stretch items to fill the container.";
+      break;
+    default:
+      msg = "Unknow property : " + alignContent.value;
+  }
+  if (!msg.startsWith("Unknow property"))
+    msg = msg + " Note: doesn't work with nowrap.";
+  updatePropDesc(msg);
 });
+
+const cmwElements = document.getElementsByClassName("container");
+// Ensure at least one element exists
+if (cmwElements.length > 0) {
+  const cmw = cmwElements[0].offsetWidth; // Access the first element's width
+  document.getElementById("container-width-input").value = cmw;
+}
+
+//update container width
+const cwi = document.getElementById("container-width-input");
+cwi.addEventListener("change", function () {
+  cmwElements[0].style.width = cwi.value + "px";
+});
+
+// Container height
+const cmhElements = document.getElementsByClassName("container");
+// Ensure at least one element exists
+if (cmhElements.length > 0) {
+  const cmh = cmhElements[0].offsetHeight; // Access the first element's height
+  document.getElementById("container-height-input").value = cmh;
+}
+
+//update container width
+const chi = document.getElementById("container-height-input");
+chi.addEventListener("change", function () {
+  cmhElements[0].style.height = chi.value + "px";
+});
+
+// flex item properties
+
+// flex-basis - Defines the starting width of an item before flex-grow or flex-shrink applies.
+const flexBasis = document.getElementById("flex-basis");
+flexBasis.addEventListener("change", function () {
+  // implement the style on very first and last item in flex container
+  let t = document.getElementsByClassName("box");
+  
+  //t[t.length - 1].style.flexBasis = flexBasis.value + "px";
+  //t[0].style.flexBasis = flexBasis.value + "px";
+
+  for( let i = 0; i < t.length; i++) {
+    t[i].style.flexBasis = flexBasis.value + "px";
+  }
+
+  let msg =
+    "Specifies the initial main size of a flex item before any available space is distributed.";
+  updatePropDesc(msg);
+});
+
+// flex-grow - Defines how much an item should grow relative to others.
+const flexGrow = document.getElementById("flex-grow");
+flexGrow.addEventListener("change", function () {
+  // implement the style on very first and last item in flex container
+  let t = document.getElementsByClassName("box");
+  t[t.length - 1].style.flexGrow = flexGrow.value / 2;
+  t[0].style.flexGrow = flexGrow.value;
+
+  let msg =
+    "Grows flex item relative to other flex items when there's available space in flex container.";
+  updatePropDesc(msg);
+});
+
+// flex-shrink - Defines how much an item should shrink if the container gets smaller.
+const flexShrink = document.getElementById("flex-shrink");
+flexShrink.addEventListener("change", () => {
+  // implement the style on very first and last item in flex container
+  let t = document.getElementsByClassName("box");
+  t[t.length - 1].style.flexShrink = flexShrink.value / 2;
+  t[0].style.flexShrink = flexShrink.value;
+
+  let msg =
+    "Defines how much a flex item will shrink relative to other items when there is insufficient in the flex container.";
+  updatePropDesc(msg);
+});
+
+// flex-order: Defines the
+const flexOrder = document.getElementById("flex-order");
+const flexOrderChange = () => {
+  let t = document.getElementsByClassName("box");
+  t[0].style.order = flexOrder.value;
+  //t[t.length - 1].style.order = flexOrder / 2;
+
+  let msg =
+    "Defines the order of flex items inside the container. Lower value appears first, default is 0.";
+  updatePropDesc(msg);
+};
+flexOrder.addEventListener("change", flexOrderChange);
+
+// align-self (Overrides align-items styling for Individual Items) - Allows an individual item to have a different vertical alignment.
+const flexAlignSelf = document.getElementById("align-self");
+const flexAlignSelfFunction = () => {
+  let t = document.getElementsByClassName("box");
+  // override for all items
+  for (let i = 0; i < t.length; i++) {
+    t[i].style.alignSelf = flexAlignSelf.value;
+  }
+
+  let msg =
+    "Overrides align-items from the container. Works on the cross-axis (vertical in flex-direction: row). Default: auto (inherits from align-items of the parent).";
+
+  updatePropDesc(msg);
+};
+flexAlignSelf.addEventListener("change", flexAlignSelfFunction);
